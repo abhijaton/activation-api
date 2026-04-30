d2eac253db33ee5216acd158670be52f5c2dc1a3
Abhijaton <abhijaton@gmail.com>
2026-04-30 11:16:01 +0530

    fix routing

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Activation API", Version = "v1" });
-});
-builder.Services.AddCors(o => o.AddPolicy("AllowAll", b => 
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", b =>
     b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Activation API v1"));
+app.UseSwaggerUI();
 app.UseCors("AllowAll");
+app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
